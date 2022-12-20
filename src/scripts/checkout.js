d9bcb8423fd5e637614af4694170397d55500355
kasulaarunteja <kasulaarunteja@gmail.com>
2022-12-20 21:17:34 +0530

    check

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -1,7 +1,6 @@
+document.querySelector('#place-order').addEventListener('click', function () {
+  localStorage.removeItem('cart')
 
-document.querySelector("#place-order").addEventListener("click", function(){
-    localStorage.removeItem("cart")
-
-    document.getElementById("order-message").innerText = "Your order is successfully placed"
+  document.getElementById('order-message').innerText =
+    'Your order is successfully placed'
 })
-
